d9c73b0222d834426910fbb46629c7db1a94fa5c
Erik Kvale <eirikval@gmail.com>
2017-06-19 13:15:21 -0700

    Initial commit

---
+++ datasources/mssql.py
@@ -0,0 +1,74 @@
+"""
+Connecting to MS SQL Server
+
+Special guest(s):
+    sqlalchemy: http://docs.sqlalchemy.org/en/rel_1_1/dialects/mssql.html
+    pyodbc:
+
+"""
+import pandas
+from sqlalchemy import create_engine
+from urllib import parse
+import pyodbc
+
+class MSSqlDb:
+    """
+    Handler to an MS SQL Server database and its contents
+    """
+
+    def __init__(self, connection_dict):
+        """ Initializes SQLAlchemy engine, using pyodbc as the DBAPI and the connection dictionary provided"""
+        self.connection_dict = connection_dict
+        self.engine = create_engine("mssql+pyodbc:///?odbc_connect={0}".format(self.dict_to_url()))
+
+
+    def dict_to_url(self):
+        connection_string = ("DRIVER={0};"
+                             "SERVER={1};"
+                             "DATABASE={2};"
+                             "Trusted_Connection={3}".
+                             format(self.connection_dict['DRIVER'],
+                                    self.connection_dict['SERVER'],
+                                    self.connection_dict['DATABASE'],
+                                    self.connection_dict['Trusted_Connection']))
+        connection_url = parse.quote_plus(connection_string)
+        return connection_url
+
+
+    def bulk_insert(self, csv_file_path, sql_schema_name, sql_table_name,
+                    field_terminator, row_terminator):
+        """
+        EXAMPLE:
+
+        MSSqlDb.bulk_insert(self,
+                    csv_file_path=r'C:\mycsv.csv',
+                    sql_schema_name='dbo'
+                    sql_table_name='mytable',
+                    field_terminator=',',
+                    row_terminator='\\n'
+                    )
+        """
+        sql = ("BULK INSERT [{0}].[{1}]\n"
+               "FROM '{2}'\n"
+               "WITH (FIELDTERMINATOR = '{3}', ROWTERMINATOR = '{4}');".format(
+                   sql_schema_name,
+                   sql_table_name,
+                   csv_file_path,
+                   field_terminator,
+                   row_terminator
+               ))
+
+        # This is the only implementation that works at the moment in
+        # issuing and committing the BULK INSERT functionality successfully
+        # in SQL Server (2016), otherwise
+        raw_con = self.engine.raw_connection()
+        cursor = raw_con.cursor()
+        cursor.execute(sql)
+        cursor.commit()
+        cursor.close()
+
+
+if __name__ == '__main__':
+    """
+    Usage
+    """
---
+++ main.py
@@ -0,0 +1,68 @@
+import csv
+import os
+import tempfile
+import pandas as pd
+from datasources.mssql import MSSqlDb
+from pandas.errors import ParserError
+
+
+def chunky_monkey_csv_loader(csv_file_path,
+                             sqlalchemy_engine,
+                             bulk_insert=True,
+                             included_columns=None,
+                             chunk_size=10000):
+    """
+    Reads the specified csv file by chunks, each chunk is a Pandas dataframe
+    and is loaded in to the specified SQL database by each unit chunk. This
+    chunking is specified in the documentation for the Pandas.Dataframe.read_csv()
+    method and the iterator object returned when 'chunksize' is specified. If
+    bulk_insert=True, then utilize the SQL flavor's bulk insert method if available.
+    """
+
+    chunk_iterator = pd.read_csv(filepath_or_buffer=csv_file_path,
+                                 usecols=included_columns,
+                                 iterator=True,
+                                 chunksize=chunk_size)
+    for chunk in chunk_iterator:
+        if bulk_insert:
+
+            try:
+                # delete=False and manual close and delete for Windows NT:
+                # https://docs.python.org/3/library/tempfile.html
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_csv:
+                    chunk.to_csv(temp_csv.name, header=False)
+                    temp_csv.close()
+                    sql_db.bulk_insert(csv_file_path=temp_csv.name,
+                                       sql_schema_name='dbo',
+                                       sql_table_name='TREE',
+                                       field_terminator=',',
+                                       row_terminator='\\n')
+
+            except Exception as e:
+                print(e)
+            except ParserError as p:
+                print('Check that the file is not malformed')
+            finally:
+                os.unlink(temp_csv.name)
+        else:
+            chunk.to_sql()
+
+
+if __name__=='__main__':
+
+    big_csv_file = r'C:\Users\Erik\Documents\University of Idaho\Projects\data\data\TREE.csv'
+
+    conn_dict = {
+        'DRIVER': 'ODBC Driver 13 for SQL Server',
+        'SERVER': 'ORION\TARS',
+        'DATABASE': 'Data_Raju',
+        'Trusted_Connection': 'yes'
+    }
+
+    sql_db = MSSqlDb(connection_dict=conn_dict)
+
+    columns_to_include = ['PLT_CN', 'INVYR', 'CONDID', 'STATECD', 'CYCLE', 'COUNTYCD', 'PLOT']
+
+    chunky_monkey_csv_loader(csv_file_path=big_csv_file,
+                             sqlalchemy_engine=sql_db.engine,
+                             included_columns=columns_to_include)
